bd3037cc49eb9a4612adacece98b238697f5a62c
Cristian David Home <cristian_david033@hotmail.com>
2024-08-08 18:04:47 -0500

    Change name and path.

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/wanapo-content-game/',
+  base: '/wanapo-content-game2/',
   plugins: [
     Icons({ compiler: 'vue3' }),
     vue(),
